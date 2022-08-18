7a7b6ed2d0b99b330d884990e35749f802b0919f
Mesut Tunçer <53650821+massout@users.noreply.github.com>
2022-08-18 12:12:43 +0300

    Create client.hpp

---
+++ client.hpp
@@ -0,0 +1,48 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <string>
+
+// Data yapısının içerisini serverla aynı olması şartıyla istediğiniz gibi doldurun.
+struct Data {};
+
+class Client {
+    int sock = 0, client_fd;
+    struct sockaddr_in serv_addr;
+
+   public:
+    Client(std::string &_addr, uint8_t _port);
+    Data get_data(Data &_data);
+    ~Client();
+};
+
+Client::Client(std::string &_addr, uint8_t _port) {
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(_port);
+
+    inet_pton(AF_INET, &_addr[0], &serv_addr.sin_addr);
+
+    client_fd = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+}
+
+Data Client::get_data(Data &_data) {
+    Data _buffer;
+
+    read(sock, &_buffer, sizeof(_data));
+
+    return _buffer;
+}
+
+Client::~Client() {
+    close(client_fd);
+}
+
+#endif
