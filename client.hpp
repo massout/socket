cc15d7b67e82d1e613f7e5a82ddd94e543f32025
Mesut Tunçer <53650821+massout@users.noreply.github.com>
2022-08-26 18:49:59 +0300

    Added template

--- client.hpp
+++ client.hpp
@@ -1,5 +1,4 @@
-#ifndef CLIENT_HPP
-#define CLIENT_HPP
+#pragma once
 
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -7,20 +6,19 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-// Data yapısının içerisini serverla aynı olması şartıyla istediğiniz gibi doldurun.
-struct Data {};
-
+template <typename T>
 class Client {
     int sock = 0, client_fd;
     struct sockaddr_in serv_addr;
 
    public:
     Client(const char *_addr, uint16_t _port);
-    Data get_data(Data &_data);
+    T get_data(T &_data);
     ~Client();
 };
 
-Client::Client(const char *_addr, uint16_t _port) {
+template <typename T>
+Client<T>::Client(const char *_addr, uint16_t _port) {
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
     serv_addr.sin_family = AF_INET;
@@ -31,7 +29,8 @@ Client::Client(const char *_addr, uint16_t _port) {
     client_fd = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 }
 
-Data Client::get_data(Data &_data) {
+template <typename T>
+T Client<T>::get_data(T &_data) {
     Data _buffer;
 
     read(sock, &_buffer, sizeof(_data));
@@ -39,8 +38,7 @@ Data Client::get_data(Data &_data) {
     return _buffer;
 }
 
-Client::~Client() {
+template <typename T>
+Client<T>::~Client() {
     close(client_fd);
 }
-
-#endif
